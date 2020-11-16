c0753a4760045e74b7272b9e5ce40cda8f174521
Josie Eshkenazi <jeshkena@uwaterloo.ca>
2020-11-16 13:21:54 -0500

    write to csv

--- Modelling/CarbonFootprint/main.py
+++ Modelling/CarbonFootprint/main.py
@@ -1,13 +1,18 @@
+import argparse
 import os
+import csv
 import matplotlib.pyplot as plt
 import pandas as pd
-from matplotlib.dates import DateFormatter, DayLocator, datestr2num
+from matplotlib.dates import DateFormatter, DayLocator, date2num
 from datetime import datetime, date
 
 from dotenv import load_dotenv
 from model import getCarbonFootprint
 from shopify import getOrders, getFulfillment
 from typing import Tuple
+
+
+
 
 
 
@@ -19,28 +24,45 @@ API_PASSWORD = os.getenv("API_PASSWORD")
 DOMAIN = os.getenv("DOMAIN")
 
 
+def writeToCSV(csv_filename, data):
+	with open(csv_filename, 'w') as f:
+		# Write to text file
+		writer = csv.writer(f, delimiter='\t')
+		
+		writer.writerow(["date", "grams_carbon"])
+		for _date, grams_carbon in data.items():
+			writer.writerow([_date, grams_carbon])
+
+
 if __name__ == "__main__":
+	# Parse command line arguments
+	parser = argparse.ArgumentParser("Calculate Carbon Footprint")
+	parser.add_argument("csv_filename", type=str, help="Absolute path to the csv file to write data to.")
+	args = parser.parse_args()
+
 	# Get the orders from the shop
 	orders = getOrders(DOMAIN, API_KEY, API_PASSWORD)
 
 	date_range = pd.date_range(end=datetime.utcnow(), periods=100)
-	carbon_footprints = {datestr2num(_date.strftime("%Y-%m-%d")): 0.0 for _date in date_range}
+	carbon_footprints = {_date.date(): 0.0 for _date in date_range}
 	
 	
 	for order in orders:
 		carbon_footprint = getCarbonFootprint(order)
 		_date = datetime.fromisoformat(order.created_at)
 		utc_date = _date - _date.utcoffset()
-		day = datestr2num(utc_date.strftime("%Y-%m-%d"))
+		day = utc_date.date()
 		
 		carbon_footprints[day] += carbon_footprint
+
+	writeToCSV(args.csv_filename, carbon_footprints)
 
 	x, y = zip(*carbon_footprints.items())
 	fig = plt.figure()
 	fig.suptitle("Carbon Footprint Over Time")
 
 	# Setup formatting of dates
-	plt.plot_date(x, y, fmt="bo", tz=None, xdate=True)	
+	plt.plot_date(date2num(x), y, fmt="bo", tz=None, xdate=True)	
 	
 	plt.xlabel("Date")
 	plt.ylabel("CO2 [g]")

50f654b19d8243b588eb9f2a2b98cbb940313d0c
allysonaberg <allya7897@gmail.com>
2020-11-16 16:57:30 -0500

    change file structure

--- prototype/app.js
+++ prototype/app.js
@@ -21,38 +21,26 @@ app.get('/', (req, res) => {
 	var retrievedData;
 	var processedDataArray = []
 	// spawn new child process to call the python script
-	const python = spawn('python', ['/Modelling/CarbonFootprint/main.py']);
+	const python = spawn('python', [__dirname+'/Modelling/CarbonFootprint/main.py']);
 
-	// python.stdout.on('data', function (data) {
-	// 	console.log('grabbing data from script ...');
-	// 	 retrievedData = data.toString();
-		 
-	// 	 var dataArray = retrievedData.split(',')
+	python.stdout.on('data', function (data) {
+	 console.log('grabbing data from script ...');
+	 retrievedData = data.toString();
+	});
+	// in close event we are sure that stream from child process is closed
+	python.on('close', (code) => {
+		
+		var dataArray = retrievedData.split(',')
 
-	// 	 //THIS IS A HACK, FIX L8R
-	// 	 for (var i = 0; i < dataArray.length; i+=2) {
-	// 		 processedDataArray.push([dataArray[i].replace('\r', '').replace('\n', ''), dataArray[i+1]])
-	// 	 }
- 
-	// 	 DB.writeToDB(processedDataArray)
-	// 	 DB.readFromDB()	
-	// 	//  res.render('./index.html', {data: processedDataArray});
-	// });
-	res.render('./index.html', {data: "test"});
-	// // in close event we are sure that stream from child process is closed
-	// python.on('close', (code) => {
+		//THIS IS A HACK, FIX L8R
+		for (var i = 0; i < dataArray.length; i+=2) {
+			processedDataArray.push([dataArray[i].replace('\r', '').replace('\n', ''), dataArray[i+1]])
+		}
 
-	// 	var dataArray = retrievedData.split(',')
-
-	// 	//THIS IS A HACK, FIX L8R
-	// 	for (var i = 0; i < dataArray.length; i+=2) {
-	// 		processedDataArray.push([dataArray[i].replace('\r', '').replace('\n', ''), dataArray[i+1]])
-	// 	}
-
-	// 	DB.writeToDB(processedDataArray)
-	// 	DB.readFromDB()	
-	// 	res.render('./index.html', {data: processedDataArray});
-	// });
+		DB.writeToDB(processedDataArray)
+		DB.readFromDB()	
+		res.render('./index.html', {data: processedDataArray});
+	});
 
 })
 

a9b707ef26b24aaad0401875f12bc157a93d5bea
allysonaberg <allya7897@gmail.com>
2020-11-16 18:06:39 -0500

    add /

--- prototype/app.js
+++ prototype/app.js
@@ -18,7 +18,7 @@ const apiPassword = process.env.API_PASSWORD;
 
 	
 app.get('/', (req, res) => {
-	res.render(__dirname+'views/index.html', {data: "hiii"});
+	res.render(__dirname+'/views/index.html', {data: "hiii"});
 	var retrievedData;
 	var processedDataArray = []
 	// spawn new child process to call the python script

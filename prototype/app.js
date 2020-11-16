79ac2b046572a67609d5c95de50fbeb30d0e9cb4
allysonaberg <allya7897@gmail.com>
2020-11-16 16:46:03 -0500

    don't rely on python close

--- prototype/app.js
+++ prototype/app.js
@@ -18,28 +18,41 @@ const apiPassword = process.env.API_PASSWORD;
 
 	
 app.get('/', (req, res) => {
-	var retrievedData
+	var retrievedData;
 	var processedDataArray = []
 	// spawn new child process to call the python script
 	const python = spawn('python', ['../Modelling/CarbonFootprint/main.py']);
+
 	python.stdout.on('data', function (data) {
-	 console.log('grabbing data from script ...');
-	retrievedData = data.toString();
+		console.log('grabbing data from script ...');
+		 retrievedData = data.toString();
+		 
+		 var dataArray = retrievedData.split(',')
+
+		 //THIS IS A HACK, FIX L8R
+		 for (var i = 0; i < dataArray.length; i+=2) {
+			 processedDataArray.push([dataArray[i].replace('\r', '').replace('\n', ''), dataArray[i+1]])
+		 }
+ 
+		 DB.writeToDB(processedDataArray)
+		 DB.readFromDB()	
+		 res.render('./index.html', {data: processedDataArray});
 	});
-	// in close event we are sure that stream from child process is closed
-	python.on('close', (code) => {
+	
+	// // in close event we are sure that stream from child process is closed
+	// python.on('close', (code) => {
 
-		var dataArray = retrievedData.split(',')
+	// 	var dataArray = retrievedData.split(',')
 
-		//THIS IS A HACK, FIX L8R
-		for (var i = 0; i < dataArray.length; i+=2) {
-			processedDataArray.push([dataArray[i].replace('\r', '').replace('\n', ''), dataArray[i+1]])
-		}
+	// 	//THIS IS A HACK, FIX L8R
+	// 	for (var i = 0; i < dataArray.length; i+=2) {
+	// 		processedDataArray.push([dataArray[i].replace('\r', '').replace('\n', ''), dataArray[i+1]])
+	// 	}
 
-		DB.writeToDB(processedDataArray)
-		DB.readFromDB()	
-		res.render('./index.html', {data: processedDataArray});
-	});
+	// 	DB.writeToDB(processedDataArray)
+	// 	DB.readFromDB()	
+	// 	res.render('./index.html', {data: processedDataArray});
+	// });
 
 })
 
@@ -49,5 +62,5 @@ app.post('/', (req, res) => {
 
 //START
 app.listen(port, () => {
-	console.log("listening at port" + port)
+	console.log("listening at port " + port)
 })

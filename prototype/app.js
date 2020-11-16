69505a1e2d25209eabbcfafdf7993bc11adeba62
allysonaberg <allya7897@gmail.com>
2020-11-16 13:10:30 -0500

    moving out db stuff

--- prototype/app.js
+++ prototype/app.js
@@ -2,37 +2,22 @@ const express = require('express')
 const {spawn} = require('child_process')
 const bodyParser = require('body-parser')
 const sqlite3 = require('sqlite3').verbose();
-
 require('dotenv/config')
+var DB = require('./db/db_methods')
 
 const app = express()
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json())
+
 const port = process.env.PORT || 5000;
 const apiKey = process.env.API_KEY;
 const apiPassword = process.env.API_PASSWORD;
 
-/* Database object */
-let db = new sqlite3.Database('./db/pickled_herring.db', (err) => {
-	if (err) {
-	  return console.error(err.message);
-	}
-	console.log('Connected to the in-memory SQlite database.');
-  });
-
-
-let sql = `SELECT * FROM footprints`;
-
-/* First returned row only */
-db.get(sql, (err, row) => {
-	if (err) {
-		console.error(err.message);
-	}
-		row ? console.log(row.client_id, row.date, row.grams_carbon) : console.log(`No data found.`);
-
-	});
-
-
+	
 app.get('/', (req, res) => {
+	DB.readFromDB()
 	var dataToSend;
 	// spawn new child process to call the python script
 	const python = spawn('python', ['python_test.py']);
@@ -42,47 +27,17 @@ app.get('/', (req, res) => {
 	});
 	// in close event we are sure that stream from child process is closed
 	python.on('close', (code) => {
-	// send data to browser
-	res.send(dataToSend)
-	console.log(dataToSend)
+		var fakedata = "this is fakedata"
+		res.render('./index.html', {data: fakedata});
+		console.log(dataToSend)
 	});
-	
 })
 
 app.post('/', (req, res) => {
 	console.log(req.body)
-	// var order = new Order({
-	// 	date: req.body.order_date,
-	// 	footprint: req.body.footprint
-	// });
-
-	// order.save()
-	// 	.then(item => {
-	// 		res.send("SAVED")
-	// 	})
-	// 	.catch(err => {
-	// 		console.log("ERRPR")
-	// 	})
 });
-
-app.get('/index', (req, res) => {
-	res.sendFile('./index.html', {root: __dirname});
-});
-
-
-
-
 
 //START
 app.listen(port, () => {
 	console.log("listening at port" + port)
 })
-
-
-/* Close the database connection */
-db.close((err) => {
-	if (err) {
-	  return console.error(err.message);
-	}
-	console.log('Close the database connection.');
-});
---
+++ prototype/db/db_methods.js
@@ -0,0 +1,36 @@
+const sqlite3 = require('sqlite3').verbose();
+
+function readFromDB() {
+	/* Database object */
+	let db = new sqlite3.Database('./db/pickled_herring.db', (err) => {
+		if (err) {
+		return console.error(err.message);
+		}
+		console.log('Connected to the in-memory SQlite database.');
+	});
+
+
+	let sql = `SELECT * FROM footprints`;
+
+	/* First returned row only */
+	db.get(sql, (err, row) => {
+		if (err) {
+			console.error(err.message);
+		}
+			row ? console.log(row.client_id, row.date, row.grams_carbon) : console.log(`No data found.`);
+
+		});
+
+	/* Close the database connection */
+	db.close((err) => {
+		if (err) {
+		return console.error(err.message);
+		}
+		console.log('Close the database connection.');
+	});
+
+}
+
+
+
+module.exports = { readFromDB }

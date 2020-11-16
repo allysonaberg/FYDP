37929e7d9818e61c94d183c4078939416a20adce
allysonaberg <allya7897@gmail.com>
2020-11-16 16:52:35 -0500

    testing, remove python dependency

--- prototype/app.js
+++ prototype/app.js
@@ -23,22 +23,22 @@ app.get('/', (req, res) => {
 	// spawn new child process to call the python script
 	const python = spawn('python', ['/Modelling/CarbonFootprint/main.py']);
 
-	python.stdout.on('data', function (data) {
-		console.log('grabbing data from script ...');
-		 retrievedData = data.toString();
+	// python.stdout.on('data', function (data) {
+	// 	console.log('grabbing data from script ...');
+	// 	 retrievedData = data.toString();
 		 
-		 var dataArray = retrievedData.split(',')
+	// 	 var dataArray = retrievedData.split(',')
 
-		 //THIS IS A HACK, FIX L8R
-		 for (var i = 0; i < dataArray.length; i+=2) {
-			 processedDataArray.push([dataArray[i].replace('\r', '').replace('\n', ''), dataArray[i+1]])
-		 }
+	// 	 //THIS IS A HACK, FIX L8R
+	// 	 for (var i = 0; i < dataArray.length; i+=2) {
+	// 		 processedDataArray.push([dataArray[i].replace('\r', '').replace('\n', ''), dataArray[i+1]])
+	// 	 }
  
-		 DB.writeToDB(processedDataArray)
-		 DB.readFromDB()	
-		 res.render('./index.html', {data: processedDataArray});
-	});
-	
+	// 	 DB.writeToDB(processedDataArray)
+	// 	 DB.readFromDB()	
+	// 	//  res.render('./index.html', {data: processedDataArray});
+	// });
+	res.render('./index.html', {data: "test"});
 	// // in close event we are sure that stream from child process is closed
 	// python.on('close', (code) => {
 

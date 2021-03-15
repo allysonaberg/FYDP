b598e4b08f90143e36cfd96847df8dd8264b711e
tanyarllrm <trrallia@uwaterloo.ca>
2021-03-14 21:55:05 -0400

    change analyses and suggestions to dict, add avgs template

---
+++ prototype/content/item_averages.js
@@ -0,0 +1,8 @@
+// average emissions for an item type in kg CO2
+item_averages = {
+    "tshirt": 10,
+    "jeans": 18,
+    "bodysuit": 12,
+    "sweater": 24
+}
+module.exports = item_averages;
--- prototype/content/suggestions.js
+++ prototype/content/suggestions.js
@@ -3,4 +3,11 @@ suggestions = [
     "Sample suggestion 2",
     "Sample suggestion 3"
 ]
+
+suggestions_dict = {
+    "fabric1": "suggestion1",
+    "fabric2": "suggestion2",
+    "fabric3": "suggestion3"
+}
+
 module.exports = suggestions;

92f43f1752865a3bbc9b3a3bf612aad8db6bfebe
tanyarllrm <trrallia@uwaterloo.ca>
2021-03-15 22:59:28 -0400

    rank calculation

--- prototype/calculations/rank.js
+++ prototype/calculations/rank.js
@@ -1,6 +1,15 @@
-function calculate_rank(kg_carbon) {
+const item_averages = require('../content/item_averages');
+const footprint = require('./footprint');
+
+// RANK A: -10% or less than avg
+// RANK B: between -10% and +5% of avg
+// RANK C: between +5% and +25% of avg
+// RANK D: +25% or more than avg
+
+function calculate_rank(kg_carbon, item_type) {
     // TODO calculate rank from kg_carbon 
     var rank = 0;
+    
     return rank;
 }
 

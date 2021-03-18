aba7d9186026926cdba3b88cda48345bd0d78ed6
tanyarllrm <trrallia@uwaterloo.ca>
2021-03-18 11:03:21 -0400

    update avg values

--- prototype/calculations/rank.js
+++ prototype/calculations/rank.js
@@ -1,15 +1,25 @@
 const item_averages = require('../content/item_averages');
-const footprint = require('./footprint');
 
 // RANK A: -10% or less than avg
 // RANK B: between -10% and +5% of avg
 // RANK C: between +5% and +25% of avg
 // RANK D: +25% or more than avg
 
-function calculate_rank(kg_carbon, item_type) {
-    // TODO calculate rank from kg_carbon 
-    var rank = 0;
-    
+function calculate_rank(kg_carbon, item_type) { 
+    var rank = '';
+    var avg = item_averages[item_type];
+    var pcdiff = ((avg - kg_carbon)/avg)*100
+
+    if (pcdiff <= -10.0){
+        rank = 'A';
+    } else if (pcdiff > -10.0 && pcdiff <= 5.0){
+        rank = 'B';
+    } else if (pcdiff > 5.0 && pcdiff <= 25.0 ){
+        rank = 'C';
+    } else {
+        rank = 'D';
+    }
+
     return rank;
 }
 
--- prototype/content/item_averages.js
+++ prototype/content/item_averages.js
@@ -1,8 +1,10 @@
-// average emissions for an item type in kg CO2
+// average emissions for an item type in kg CO2, from Mistra Future Fashion & extrapolation
 item_averages = {
-    "tshirt": 10,
-    "jeans": 18,
-    "bodysuit": 12,
-    "sweater": 24
+    "tshirt": 2,
+    "jeans": 10.5,
+    "bodysuit": 3,
+    "sweater": 16,
+    "dress": 16.5,
+    "jacket": 17.5,
 }
 module.exports = item_averages;

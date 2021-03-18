710810044c8678159707422784ab46d7a294276a
tanyarllrm <trrallia@uwaterloo.ca>
2021-03-18 19:51:29 -0400

    suggestions

--- prototype/content/suggestions.js
+++ prototype/content/suggestions.js
@@ -1,13 +1,9 @@
-suggestions = [
-    "Sample suggestion 1",
-    "Sample suggestion 2",
-    "Sample suggestion 3"
-]
-
-suggestions_dict = {
-    "fabric1": "suggestion1",
-    "fabric2": "suggestion2",
-    "fabric3": "suggestion3"
+suggestions = {
+    "general": "Utilizing recycled textiles on average can eliminate the use of 6000L of water, 300g of fertilizer and 200g of pesticides.",
+    "cotton": "When sourcing cotton, consider cotton with an OCCGUARANTEE certification. Cotton with this certification is 100% organic with zero dyes producing approximately 46% less carbon emissions than conventional cotton.",
+    "polyester": "Consider swaping polyester to a biosynthetic fiber that uses renewable raw materials or biowaste as inputs instead of petroleum.  ",
+    "acrylic": "Consider sourcing fabrics and yarns made from natural fibres such as cotton or wool instead of acrylic for less emissions and a longer garment life. Alternatively, recyled polyester is a lower cost option. ",
+    "nylon": "Newer fabrics now available include cotton technologies which can give apparel the desired qualities of nylon such as stretch and moisture-wicking, while being more sustainable, durable, and comfortable fot the wearer. "
 }
 
-module.exports = suggestions_dict;
+module.exports = suggestions;
